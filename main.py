3c9a42084e63664100d1ef3e696b94dea9ce86cc
Logan Cox <mail@logan-cox.com>
2022-09-25 19:28:46 +0100

    adding inventory size and cost properties

--- main.py
+++ main.py
@@ -70,6 +70,37 @@ Inventory New Products:
     # Remove product from our inventory
     inventory_1.remove_product(product_1)
     print(f"""
-New Inventory After Product has been removed:
+New inventory after Product has been removed:
 {[product.name for product in inventory_1.products]}
     """)
+
+    # Remove multiple products from our inventory
+    product_3 = Product(
+        product_id=3,
+        name="DR pepper",
+        price=1.35,
+        quantity=50
+    )
+
+    inventory_1.add_product(product_3)
+
+    inventory_1.remove_products([product_3, product_2])
+
+    print(f"""
+New inventory after multiple products have been been removed:
+{[product.name for product in inventory_1.products]}
+    """)
+
+    # Add multiple products to our already instantiated inventory
+    inventory_1.add_products([product_1, product_2, product_3])
+    print(f"""
+    New inventory after multiple products have been been added:
+{[product.name for product in inventory_1.products]}
+        """)
+
+    # Show the total size of our inventory
+    print(f"Total inventory size: {inventory_1.total_items}")
+
+    # Show the total amount all products in our inventory would cost
+    print(f"Total inventory cost: ${inventory_1.inventory_sum}")
+
--- product_inventory/inventory.py
+++ product_inventory/inventory.py
@@ -5,13 +5,12 @@ class Inventory:
     """
     A class to represent our inventory for a store
     """
+
     def __init__(self, name: str, products=None):
-        if products is None:
+        if not products:
             products = []
         self._name: str = name
         self._products: list[Product] = products
-        self._total_items: int = len(self._products)
-        self._inventory_sum: float = sum([(p.price * p.quantity) for p in self._products])
 
     @property
     def name(self) -> str:
@@ -29,26 +28,30 @@ class Inventory:
     def products(self, new_products: list[Product]):
         self._products = new_products
 
-    def add_product(self, new_product: Product) -> None:
-        self._products.append(new_product)
-        return None
+    @property
+    def total_items(self) -> int:
+        return len(self._products)
 
-    def add_products(self, new_products: list[Product]) -> None:
+    @property
+    def inventory_sum(self) -> float:
+        return sum([(p.price * p.quantity) for p in self._products])
+
+    def add_product(self, new_product: Product):
+        self._products.append(new_product)
+
+    def add_products(self, new_products: list[Product]):
         for new_product in new_products:
             self._products.append(new_product)
-        return None
 
-    def remove_product(self, product_to_remove: Product) -> None:
+    def remove_product(self, product_to_remove: Product):
         try:
             self._products.remove(product_to_remove)
-        except ValueError as error:
-            raise f"Product {product_to_remove.name} not in inventory {error}"
-        return None
+        except ValueError as e:
+            raise ValueError(f"Product {product_to_remove} not in inventory. Error: {e}")
 
-    def remove_products(self, products_to_remove: list[Product]) -> None:
+    def remove_products(self, products_to_remove: list[Product]):
         for product_to_remove in products_to_remove:
             try:
                 self._products.remove(product_to_remove)
-            except ValueError as error:
-                raise f"Product {product_to_remove.name} not in inventory {error}"
-        return None
+            except ValueError as e:
+                raise ValueError(f"Product {product_to_remove} not in inventory. Error: {e}")
